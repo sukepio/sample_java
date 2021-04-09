83eccb9ae9c0dc50e79fdd54dad85e8d9f19107c
sukepio <naoto927sukena@gmail.com>
2021-04-09 10:54:48 +0900

    Add static to MAX_HP and MAX_MP in Cleric field

--- comment/Cleric.java
+++ comment/Cleric.java
@@ -3,19 +3,19 @@ import java.util.*;
 public class Cleric {
   String name;
   int hp = 50;
-  final int MAX_HP = 50;
+  static final int MAX_HP = 50;
   int mp = 10;
-  final int MAX_MP = 10;
+  static final int MAX_MP = 10;
 
   public void selfAid(){
     this.mp -= 5;
-    this.hp = this.MAX_HP;
+    this.hp = Cleric.MAX_HP;
     System.out.println(this.name + "はMPが" + this.hp + "に回復した！");
   }
 
   public int pray(int sec){
     int recover = new Random(3).nextInt(sec);
-    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+    int recoverActual = Math.min(Cleric.MAX_MP - this.mp, recover);
 
     this.mp += recoverActual;
     System.out.println(this.name + "は祈り、MPが" + recoverActual + "回復した！");
